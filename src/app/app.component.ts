b43297c7ee0c093d135fc21005f5bc7bf083792c
Brendan Malycky <bmalycky@hotmail.com>
2024-01-12 13:00:03 -0700

    finished 2 more lesssons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,8 @@ import { WishItem } from 'src/shared/models/wish-item';
 export class AppComponent {
   title = 'Wishlist';
 
-  items = [
+  // we know items is only going to be an array of wishitem objects, so we define that as its type. This fixes an error that would occur if the array was empty. The error would say Property 'wishText' does not exist on type 'never'. The items array becomes type never when its empty. Now we get a page of nothing instead of an error if there is nothing in the array //
+  items: WishItem[] = [
     // if you hover over WishItem it will tell you the isComplete property is optional, which is indicated by the ? //
     new WishItem("Learn Angular"),
     new WishItem("Get Lunch", true),
