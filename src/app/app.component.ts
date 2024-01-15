8156e2ae2800eb4cf8c26c34d0b285ebf0a25e28
Brendan Malycky <bmalycky@hotmail.com>
2024-01-15 13:56:02 -0700

    finished more lessons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { WishItem } from 'src/shared/models/wish-item';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Wishlist';
 
@@ -16,4 +17,35 @@ export class AppComponent {
     new WishItem("Get Lunch", true),
     new WishItem("Find self cutting grass")
   ];
+
+  
+  get visibleItems(): WishItem[] {
+    let value = this.listFilter;
+    if (value === "0") {
+      return this.items; 
+    } else if (value === "1") {
+      return this.items.filter(wish => wish.isComplete === false);
+    } else {
+      return this.items.filter(wish => wish.isComplete === true);
+    }
+  } 
+
+  newWishText = "";
+
+  toggleWish(item: WishItem) {
+    // toggles each items boolean for isComplete property when clicked //
+    item.isComplete = !item.isComplete;
+    console.log(item);  
+  }
+
+  addNewWish() {
+    // adds a new wish item to the items array //
+    this.items.push(new WishItem(this.newWishText));
+    // clear textbox //
+    this.newWishText = "";
+  }
+
+  // the value of list filter is coming from the value of the options attribute "value", which is a string, default is set to 0 which is "All" //
+  listFilter: string = "0";
+
 }
